6c29e564dd014d4689990e0e91a1e35bf17a077f
Caio Luiz <caiobaader@gmail.com>
2021-08-03 04:02:44 -0300

    adicionado anotações aula 3

---
+++ IntroducaoaProgramacaocomPython/aula3.py
@@ -0,0 +1,51 @@
+a = int(input('Primeiro Bimestre: '))
+if a > 10:
+        a = int(input('Você informou uma nota errada. Primeiro Bimestre: '))
+b = int(input('Segundo Bimestre: '))
+if b > 10:
+        b = int(input('Você informou uma nota errada. Segundo Bimestre: '))
+c = int(input('Terceiro Bimestre: '))
+if c > 10:
+        c = int(input('Você informou uma nota errada. Terceiro Bimestre: '))
+d = int(input('Quarto Bimestre: '))
+if d > 10:
+        d = int(input('Você informou uma nota errada. Quarto Bimestre: '))
+
+media = (a + b + c + d) / 4
+print('Média: {}' .format(media))
+
+# if a <= 10 and b <= 10 and c <= 10 and d <= 10:
+#   print('media: {}' . format(media))
+# else
+#   print('foi informado alguma nota errada')
+
+
+# a = int(input('Entre com primeiro valor valor: '))
+# b = int(input('Entre com segundo valor: '))
+
+# resto_a = a % 2
+# resto_b = b % 2
+
+# if resto_a == 0 or not resto_b > 0:
+#     print('Foi digitado um número par')
+# else:
+#     print('Nenhum número par foi digitado')
+
+
+# a = int(input('Primeiro valor: '))
+# b = int(input('Segundo valor: '))
+# c = int(input('Terceiro valor: '))
+
+# if a > b:
+#     print('O maior número é {}' .format(a))
+# else:
+#     print('o maior número é {}' .format(b))
+
+# if a > b and a > c:
+#     print('O maior número é {}' .format(a))
+# elif b > a and b > c:
+#     print('O maior número é {}' .format(b))
+# else:
+#     print('o maior número é {}' .format(c))
+
+# print('Final do Programa')
